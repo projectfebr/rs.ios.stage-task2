cf8c0db564a0fe17844b5d70e5207f69d881934d
“Aleksandr <projectfebr@gmail.com>
2021-05-16 18:05:32 +0300

    done: exercise-1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,8 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    n = (n & 0x55) << 1 | (n & 0xAA) >> 1;
+    n = (n & 0x33) << 2 | (n & 0xCC) >> 2;
+    n = (n & 0x0F) << 4 | (n & 0xF0) >> 4;
+    return n;
 }
