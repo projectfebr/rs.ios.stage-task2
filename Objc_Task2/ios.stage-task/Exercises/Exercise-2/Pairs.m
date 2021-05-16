2cd2302d8dd4b6a41b9a862ab327edbd3a2b9f25
“Aleksandr <projectfebr@gmail.com>
2021-05-16 18:30:15 +0300

    done: exercise-2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,15 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger result = 0;
+    for (int i = 0 ; i < array.count-1; i++) {
+        for (int k = i+1; k < array.count; k++) {
+            if (abs(array[i].intValue-array[k].intValue) == number.intValue) {
+                result++;
+            }
+        }
+    }
+    return result;
 }
 
 @end
